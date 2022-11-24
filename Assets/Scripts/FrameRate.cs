942dbe2dfe3eac5245a04880f022ff7cdea8f7c5
Majiro <100861756+majiro1996@users.noreply.github.com>
2022-11-24 18:03:41 +0100

    Add files via upload

---
+++ Assets/Scripts/FrameRate.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FrameRate : MonoBehaviour
+{
+    //Permite que la variable privada sea vista en el inspector.
+    [SerializeField]
+    //Cantidad de fps deseada.
+    private int framesPerSecond = 30;
+    void Start()
+    {
+        //Desactiva el Vsync.
+        QualitySettings.vSyncCount = 0;
+        //Aplica el bloqueo de fps en la cantidad puesta en la variable.
+        Application.targetFrameRate = framesPerSecond;
+    }
+}
